923f138657e8220f70eb2b091fe895cfff72b55e
Koichi Justin Sato <koichi.sato.aow@gmail.com>
2018-09-29 10:45:45 +0900

    Initial Commit

---
+++ TDDBC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDDBC
+//
+//  Created by Koichi Justin Sato on 2018/09/29.
+//  Copyright © 2018 Koichi Justin Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
