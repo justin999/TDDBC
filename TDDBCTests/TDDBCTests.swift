ccbc83042f897726f6fdff3be28372d3f320c6a4
Koichi Justin Sato <koichi.sato.aow@gmail.com>
2018-09-29 14:18:08 +0900

    Q3まで

--- TDDBCTests/TDDBCTests.swift
+++ TDDBCTests/TDDBCTests.swift
@@ -10,25 +10,95 @@ import XCTest
 @testable import TDDBC
 
 class TDDBCTests: XCTestCase {
+    
+    var machine: Machine!
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        machine = Machine()
+        
     }
 
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Q1
+    func test_コーラ出る() {
+        machine.insert()
+        let actual = machine.pushButton()
+        XCTAssertEqual("コーラ", actual)
     }
+    
+    func test_コーラ出ない() {
+        let actual = machine.pushButton()
+        XCTAssertNotEqual("コーラ", actual)
+    }
+    
+    // Q2
+    func test_100円投入() {
+        machine.insert()
+        let actual = machine.inserted
+        XCTAssertEqual(true, actual)
+    }
+    
+    // Q3
+    func test_コーラボタンを押したらコーラが出ます() {
+        machine.insert()
+        let actual = machine.pushColaButton()
+        XCTAssertEqual("コーラ", actual)
+    }
+    
+    func test_ウーロン茶ボタンを押したらウーロン茶が出ます() {
+        machine.insert()
+        let actual = machine.pushOolongTeaButton()
+        XCTAssertEqual("ウーロン茶", actual)
+    }
+    
+    func test_ジンジャーエールボタンを押したらジンジャーエールが出ます() {
+        machine.insert()
+        let actual = machine.pushGingerAleButton()
+        XCTAssertEqual("ジンジャーエール", actual)
+    }
+}
 
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+import Foundation
+class Machine {
+    // 投入金額
+    var amount: Int = 0
+    var inserted: Bool = false
+    
+    func pushButton() -> String? {
+        if inserted {
+            return "コーラ"
+        } else {
+            return nil
         }
     }
-
+    
+    func insert() {
+        self.inserted = true
+    }
+    
+    func pushColaButton() -> String? {
+        if inserted {
+            return "コーラ"
+        } else {
+            return nil
+        }
+    }
+    
+    func pushOolongTeaButton() -> String? {
+        if inserted {
+            return "ウーロン茶"
+        } else {
+            return nil
+        }
+    }
+    
+    func pushGingerAleButton() -> String? {
+        if inserted {
+            return "ジンジャーエール"
+        } else {
+            return nil
+        }
+    }
 }
